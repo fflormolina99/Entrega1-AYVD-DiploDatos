4baee7317565b97c9a00d4c3b6dd182678331bd4
Antiloope <rodrigopizarro1234@gmail.com>
2022-04-06 21:36:57 -0300

    Se completó el ejercicio 1

--- ej1.py
+++ ej1.py
@@ -50,28 +50,37 @@ if 'cured_programming_languages' not in relevant_columns:
     relevant_columns.append('cured_programming_languages') 
 
 
-# Duplicate each row of df for each programming language
-# mentioned in the response.
-# We only include in df_lang the columns we are going to analyze later, so we
-# don't duplicate innecesary information.
-
-# df_lang = filter.cured_programming_languages\
-#     .apply(pd.Series).stack()\
-#     .reset_index(level=-1, drop=True).to_frame()\
-#     .join(filter[relevant_columns])\
-#     .rename(columns={0: 'programming_language'})
-
+# Se seleccionan los 10 conjuntos de lenguajes más populares
 language_count = filter.cured_programming_languages.value_counts()\
     .reset_index()\
     .rename(columns={'index': 'language', 'cured_programming_languages': 'frequency'})
-freq_list = language_count['language'][:11].to_list()
-freq_list = [x for x in freq_list if x] # Elimino unos elementos vacíos 
 
+language_count = language_count[language_count['language'].astype(bool)]
+
+freq_list = language_count['language'][:10].to_list()
+print(freq_list)
 filter_top = filter[filter['cured_programming_languages'].isin(freq_list)].reset_index()
+filter_top['tools_programming_languages'] = filter_top['tools_programming_languages'].replace('Python, Sql', 'Python, SQL').replace('SQL, Ninguno de los anteriores', 'SQL')
 
 # Plot
+# Se grafícan los 10 grupos de lenguajes más valorados económicamente de la encuesta.
 
 plt.figure(figsize=(12, 6))
 seaborn.boxplot(data=filter_top, x='salary_monthly_NETO', y='tools_programming_languages',
                 color='orangered')
 plt.ticklabel_format(style='plain', axis='x')
+
+# Se obtienen medidas del data set
+
+selected_columns_top = filter_top.filter(items=['salary_monthly_NETO', 'tools_programming_languages'])
+tmp = selected_columns_top.groupby(['tools_programming_languages']).mean().round(2).rename(columns={'salary_monthly_NETO': "Media"})
+tmp.loc[:, 'Mediana'] = selected_columns_top.groupby(['tools_programming_languages']).median().round(2).salary_monthly_NETO
+tmp.loc[:, 'Desviación std'] = selected_columns_top.groupby(['tools_programming_languages']).std().round(2).salary_monthly_NETO
+tmp.sort_values(by='Media', ascending=False)
+
+# En esta tabla se pueden ver distintas medidas de los datos en el data frame.
+# Algunas conclusiones que se pueden sacar mirando solo estos valores, pueden ser:
+#  - Para todos los lenguajes se cumple que la media es mayor a la mediana. Esto nos indica que la campana que desriben estos datos tiene su pico por debajo de la media. Una gran dispersión por lo tanto, nos va a indicar que se trata de una curva más plana y por lo tanto con mayor posibilidad de obtener un salario elevado.
+#  - Python aparece 3 veces en el top 5 de lenguajes populares mejores pagos. 
+#  - Se puede ver que los lenguajes como Python y Javascript solos, en conjunto con Java son lenguajes que se pueden utilizar en muchos tipos de proyectos, lo cual podría explicar la gran dispersión que tienen con respecto a los lenguajes de Bash y SQL que son de propositos específicos y tienen un sueldo mucho más estandarizado.
+#  - También se puede ver que los stacks con más de un lenguaje tienen dispersiones mayores, lo cual va de acuerdo al punto anterior.
